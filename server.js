92920f93aba8823cb1bf5862667327fbfb29ca9c
Yitzhak Kornbluth <ytkornbluth@hotmail.com>
2017-07-18 14:32:54 -0400

    pg

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const {DATABASE_URL} = require('./config');
+const pg = require('pg');
+
+pg.defaults.ssl = true;
 
 app.use(express.static('public'));
 app.use(morgan('common'));
@@ -12,12 +15,13 @@ let server;
 function runServer (database_url = DATABASE_URL) {
 	return new Promise ((res, rej) => {
 		const port = process.env.PORT || 8080;
-		app.database_url = database_url;
 		server = app.listen(port, () => {
 			console.log(`Server is listening on port ${port}`);
-			res();
-		})
-		.on('error', err => rej(err))
+			const pool = new pg.Pool({connectionString: database_url});
+			app.pool = pool;
+			res()
+			})
+		.on('error', err => rej(err))			
 	});
 }
 
