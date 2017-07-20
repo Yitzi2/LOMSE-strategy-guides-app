2a46edf961d007d9bc9ee1cd95c4ac5e0f75c3af
Yitzhak Kornbluth <ytkornbluth@hotmail.com>
2017-07-20 10:50:52 -0400

    Test SSL on Travis

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const morgan = require('morgan');
 const app = express();
 const {DATABASE_URL} = require('./config');
 const pg = require('pg');
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
 
 pg.defaults.ssl = true;
 
 app.use(express.static('public'));
 app.use(morgan('common'));
+app.use(bodyParser.json());
+
 let server;
+
+function doubleQuotes(string) {
+	return string.replace("'", "''");
+}
 
 function runServer (database_url = DATABASE_URL) {
 	return new Promise ((res, rej) => {
@@ -35,9 +43,23 @@ function closeServer () {
 	});
 }
 
-/*app.post('/users', (req, res) => {
-
-});*/
+app.post('/users', (req, res) => {
+	const requiredFields = ['username', 'password'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`
+			console.error(message);
+			return res.status(400).send(message);
+	    }
+	}
+	const queryText = `insert into users (username, hashedpassword) values(
+		'${doubleQuotes(req.body.username)}', 
+		'${doubleQuotes(bcrypt.hash(req.body.password, 10).then(h=>h))}');`;
+	app.pool.query(queryText)
+		.then(res => res.status(201).json({username, password}))
+		.catch(err => res.status(500).send(`Database access error: ${err}`));
+});
 
 
 if (require.main === module) {
---
+++ test/users.js
@@ -0,0 +1,94 @@
+const chai = require('chai');
+chai.use(require('chai-http'));
+const should = chai.should();
+
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+
+describe('user data tests', function () {
+	before(function(done) {
+		TEST_DATABASE_URL
+			.then(url => runServer(url))
+			.then(done);
+  	});
+
+	beforeEach(function () {
+		queryText = "delete * from users;" //Clear database for next test.
+		return app.pool.query(queryText);
+	});
+
+
+	after(function(done) {
+		queryText = "delete * from users;" //Clear database.
+		app.pool.query(queryText)
+			.then(closeServer())
+			.then(done);
+	});
+
+	describe ('users POST endpoint', function () {
+		it ('should return access code 201 and user info', function () {
+			const newUser = {username: "Order", password: "Chaos"};
+			return chai.request(app)
+				.post('/users')
+				.send(newUser)
+				.then(function (res) {
+					res.should.have.status(201);
+					res.should.be.json;
+					res.should.deep.equal.newUser;
+					const queryText = "select hashedpassword from users;"
+					const hashedPassword = app.pool.query(queryText).then(r => r)
+						.then
+					console.log(`hashed password was 
+						${app.pool.query(queryText)}`);
+						//To manually check it isn't close to plaintext.
+			});
+		});
+		it ('should not store password in plaintext', function () {
+				//This should be the only test accessing the database directly.
+			const newUser = {username: "Order", password: "Chaos"};
+			return chai.request(app)
+				.post('/users')
+				.send(newUser)
+				.then(function (res) {
+					const queryText = "select hashedpassword from users;"
+					const hashedPassword = app.pool.query(queryText)
+						.then(function (queryResult) {
+							console.log(`hashed password was ${queryResult}`);
+							queryResult.should.not.equal.newUser.password;
+						});
+			});
+		});
+		it ('should be able to create two users', function () {
+			const newUser = {username: "Order", password: "Chaos"};
+			const newUser2 = {username: "Life", password: "Death"}
+			return chai.request(app)
+				.post('/users')
+				.send(newUser)
+				.then(function (res) {
+					return chai.request(app)
+						.post('/users')
+						.send(newUser2)
+						.then(function (res2) {
+							res.should.have.status(201);
+							res.should.be.json;
+							res.should.deep.equal.newUser;
+							res2.should.have.status(201);
+							res2.should.be.json;
+							res2.should.deep.equal.newUser2;
+						});
+			});
+		});
+		it ('should return 400 and error message if username is missing', function () {
+			const newUser = {password: "Chaos"};
+			return chai.request(app)
+				.post('/users')
+				.send(newUser)
+				.then(function (res) {
+					res.should.have.status(400);
+					res.body.should.be.a("string");
+					res.body.should.equal("Missing username in request body");
+			});
+		});
+	});
+});
