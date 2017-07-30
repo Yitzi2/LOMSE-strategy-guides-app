3c4604e8ebefc71e503aacc954976bf2f7f1018b
Yitzhak Kornbluth <ytkornbluth@hotmail.com>
2017-07-30 14:19:47 -0400

    foo2

--- config.js
+++ config.js
@@ -15,7 +15,8 @@ exports.TEST_DATABASE_URL = new Promise ((resolve, reject) => {
 				'Authorization': `Bearer ${HEROKU_AUTH}`
 			}
 		};
-		request.get(options, (err, res) => resolve(res.TEST_DATABASE_URL));
+		request.get(options, (err, res) => {console.log("foo"+res);
+			resolve(res.TEST_DATABASE_URL);});
 	}
 });
 
