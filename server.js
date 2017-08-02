1633422afdedc6cca36879940f79fe667c5a36d3
Yitzhak Kornbluth <ytkornbluth@hotmail.com>
2017-08-02 12:26:05 -0400

    cookie

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const {DATABASE_URL} = require('./config');
 const pg = require('pg');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 pg.defaults.ssl = true;
 
@@ -54,25 +55,45 @@ app.post('/users', (req, res) => {
 	}
 	const username = req.body.username;
 	const password = req.body.password;
+	//To add: Entry for halfsalt.
 	if (username === "") return res.status(400).send("username cannot be blank");
-	const queryText = `insert into users (username, hashedpassword) values(
-		'${doubleQuotes(username)}', 
-		'${doubleQuotes(bcrypt.hashSync(password, 10))}');`;
-	app.db.query(queryText)
-		.then((queryRes) => res.status(201).json({
-			"username": req.body.username, 
-			"password": req.body.password
-		}))
-		.catch(
-			err => {
-				if (err.detail === 
-					`Key (username)=(${req.body.username}) already exists.`)
-					res.status(409).send("username already taken");
-				else {
-					console.log(err);
-					res.status(500).send();
-				}
-			});
+		//Half uses a callback, so that the salt can be stored in the cookie
+	bcrypt.genSalt(9, function (err, salt) {
+		if (err) {
+			console.log(err);
+			res.status(500).send();
+		}
+		else bcrypt.hash(password, salt, function (err, hash1) {
+			bcrypt.hash(hash1, 9) //From here on can be promises.
+				.then(hash2 => {
+					const queryText = `insert into users (username, hashedpassword,
+					halfsalt) values('${doubleQuotes(username)}',
+					'${doubleQuotes(hash2)}', '${doubleQuotes(salt)}');`;
+					return app.db.query(queryText);
+				})
+				.then(queryres => {res
+					.status(201)
+					.cookie("authentication", 
+						`{username: ${username}, halfhash: ${hash1}}`, {
+							maxAge: 1000*86400*365.2425*1000,
+							httpOnly: true,
+							secure: true
+						})
+					.json({
+						"username": username, 
+						"password": password
+					})})
+				.catch(err => {
+					if (err.detail === 
+						`Key (username)=(${username}) already exists.`)
+							res.status(409).send("username already taken");
+					else {
+						console.log(err);
+						res.status(500).send();
+					}
+				});
+		});
+	});
 });
 
 if (require.main === module) {
